5c17c7e23f3fd262ee8f2a4c9d6bc3edf2eb53b5
Randlii <93086288+Randlii@users.noreply.github.com>
2022-03-26 12:45:06 +0300

    1
    111

--- printf/gon_print.c
+++ printf/gon_print.c
@@ -419,3 +419,80 @@ int main (void)
     printf("s4w4p ~%4.4s` ~%4.4s` ~%4.4s` ~%4.4s` ~%4.4s`\n", "", "toto", "0123456789", "tjehurthteutuiehteute", NULL);
     printf("s4w10p ~%10.10s` ~%10.10s` ~%10.10s` ~%10.10s` ~%10.10s`\n", "", "toto", "0123456789", "tjehurthteutuiehteute", NULL);
 }
+#include "get_next_line.h"
+
+size_t		ft_strlen(const char *s)
+{
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i] != '\0')
+		++i;
+	return (i);
+}
+
+char		*ft_strnew(int size)
+{
+	char	*str;
+
+	str = (char *)malloc(sizeof(char) * (size + 1));
+	if (!str)
+		return (NULL);
+	str[size] = '\0';
+	return (str);
+}
+
+char		*ft_strjoin(char *s1, char *s2)
+{
+	char	*cated;
+	int		i;
+	int		j;
+
+	if (!s1 || !s2)
+		return (NULL);
+	cated = (char *)malloc(ft_strlen(s1) + ft_strlen(s2) + 1);
+	if (!cated)
+		return (NULL);
+	i = 0;
+	j = 0;
+	while (s1[i] != '\0')
+	{
+		cated[i] = s1[i];
+		++i;
+	}
+	free(s1);
+	while (s2[j] != '\0')
+	{
+		cated[i] = s2[j];
+		++i;
+		++j;
+	}
+	cated[i] = '\0';
+	return (cated);
+}
+
+int			get_next_line(char **line)
+{
+	char	*buf;
+	int		n;
+
+	buf = (char *)malloc(sizeof(char) * 2);
+	n = 0;
+	if (!line || (read(0, buf, 0)) < 0)
+		return (-1);
+	*line = ft_strnew(0);
+	while ((n = read(0, buf, 1)))
+	{
+		if (buf[0] == '\n')
+		{
+			free(buf);
+			return (1);
+		}
+		buf[n] = '\0';
+		*line = ft_strjoin(*line, buf);
+	}
+	free(buf);
+	return (0);
+}
